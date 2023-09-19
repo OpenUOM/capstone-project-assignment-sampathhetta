14adfd2bfcb255934140546e6dbc072454a42fc8
sampathhetta <you@example.com>
2023-09-19 10:35:23 +0530

    backend task

--- backend/database.js
+++ backend/database.js
@@ -135,7 +135,7 @@ const addStudent = async (id,name,age,hometown) => {
 }
 
 const updateStudent = async (name,age,id,hometown) => {
-    const sql = `UPDATE student SET name=?, age=? hometown=?,WHERE id=?`
+    const sql = `UPDATE student (name,age,id,hometown) values (?,?,?,?)`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [name,age,id,hometown])
